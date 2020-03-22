ca546b901abc21a3f6a71882ba1f2caaf4082ab7
Jose Hurtarte <JJHH121106@gmail.com>
2020-03-21 19:35:00 -0600

    Se arregla forma de analizarlo y se agrega metodo de el libro de bailey para luego ser modificado

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,9 +1,41 @@
 
 public class BinarySearchTree<K extends Comparable<K>,V> {
 
-	private BinarySearchTree<K,V> node;
-	private ComparableAssociation<K,V> right;
-	private ComparableAssociation<K,V> left;
+	private ComparableAssociation<K,V> node;
+	private BinarySearchTree<K,V> right;
+	private BinarySearchTree<K,V> left;
+	
+	
+	
+	/**
+     * @pre root and value are non-null
+     * @post returned: 1 - existing tree node with the desired value, or
+     *                 2 - the node to which value should be added
+     */
+    protected BinaryTree<E> locate(BinaryTree<E> root, E value)
+    {
+        E rootValue = root.value();
+        BinaryTree<E> child;
+
+        // found at root: done
+        if (rootValue.equals(value)) return root;
+        // look left if less-than, right if greater-than
+        if (ordering.compare(rootValue,value) < 0)
+        {
+            child = root.right();
+        } else {
+            child = root.left();
+        }
+        // no child there: not in tree, return this node,
+        // else keep searching
+        if (child.isEmpty()) {
+            return root;
+        } else {
+            return locate(child, value);
+        }
+    }
+	
+	
 	
 	
 	
