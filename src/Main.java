a40503dbc7bd9fc2f1c7c64227e8b044617cd1a0
Jose Hurtarte <JJHH121106@gmail.com>
2020-03-21 23:05:04 -0600

    se agregaron

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			DictionaryManager dictionary = new DictionaryManager();
+			dictionary.fillDictionary();
+			
+			
+			System.out.println(dictionary.translation());
+			
+		
+		
+
+	}
+
+}
